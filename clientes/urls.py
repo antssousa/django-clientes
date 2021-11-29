50939548ca79588bfec4f68a766306ef15cc81ba
Antonio Sousa <antssousa@gmail.com>
2021-11-29 20:36:33 -0300

    Adicionando css

--- clientes/urls.py
+++ clientes/urls.py
@@ -5,7 +5,7 @@ from .views import *
 urlpatterns = [
     path('listar', listar_clientes, name='listar_clientes'),
     path('cadastrar', cadastrar_cliente, name='cadastrar_cliente'),
-    # path('listar/<int:id>', listar_cliente_id, name='listar_cliente_id'),
-    # path('editar/<int:id>', editar_cliente, name='editar_cliente'),
-    # path('remover/<int:id>', remover_cliente, name='remover_cliente'),
+    path('listar/<int:id>', listar_cliente_id, name='listar_cliente_id'),
+    path('editar/<int:id>', editar_cliente, name='editar_cliente'),
+    path('remover/<int:id>', remover_cliente, name='remover_cliente'),
 ]
--- clientes/views.py
+++ clientes/views.py
@@ -19,5 +19,26 @@ def cadastrar_cliente(request):
     form = ClienteForm()
   return render(request, 'clientes/form_cliente.html', {'form': form})
 
+def listar_cliente_id(request, id):
+  cliente = Cliente.objects.get(id=id)
+  return render(request, 'clientes/lista_cliente.html', {'cliente': cliente})
+
+def editar_cliente(request, id):
+  cliente = Cliente.objects.get(id=id)
+  form = ClienteForm(request.POST or None, instance=cliente)
+  if form.is_valid():
+    form.save()
+    return redirect('listar_clientes')
+  return render(request, 'clientes/form_cliente.html', {'form': form})
+
+def remover_cliente(request, id):
+  cliente = Cliente.objects.get(id=id)
+  if request.method == 'POST':
+    cliente.delete()
+    return redirect('listar_clientes')
+  return render(request, 'clientes/confirma_exclusao.html', {'cliente': cliente}) 
+
+
+
 
 
