00dbfadd7b71a46c19db4bfdbbf3b119a0221d7d
Antonio Sousa <antssousa@gmail.com>
2021-12-21 19:48:42 -0300

    Adicionando autenticação de usuários

--- clientes/urls.py
+++ clientes/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('editar/<int:id>', editar_cliente, name='editar_cliente'),
     path('remover/<int:id>', remover_cliente, name='remover_cliente'),
     path('cadastrar_usuario/', cadastrar_usuario, name='cadastrar_usuario'),
-    # path('logar_usuario/', logar_usuario, name='logar_usuario'),
-    # path('deslogar_usuario/', deslogar_usuario, name='deslogar_usuario'),
+    path('logar_usuario/', logar_usuario, name='logar_usuario'),
+    path('deslogar_usuario/', deslogar_usuario, name='deslogar_usuario'),
 ]
--- clientes/views.py
+++ clientes/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import redirect, render
 from .models import Cliente
 from .forms import ClienteForm
 
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 # Create your views here.
 
@@ -11,6 +14,7 @@ def listar_clientes(request):
   clientes = Cliente.objects.all()
   return render(request, 'clientes/listar_clientes.html', {'clientes' : clientes})
 
+@login_required()
 def cadastrar_cliente(request):
   if request.method == 'POST':
     form = ClienteForm(request.POST)
@@ -25,6 +29,7 @@ def listar_cliente_id(request, id):
   cliente = Cliente.objects.get(id=id)
   return render(request, 'clientes/lista_cliente.html', {'cliente': cliente})
 
+@login_required()
 def editar_cliente(request, id):
   cliente = Cliente.objects.get(id=id)
   form = ClienteForm(request.POST or None, instance=cliente)
@@ -33,12 +38,14 @@ def editar_cliente(request, id):
     return redirect('listar_clientes')
   return render(request, 'clientes/form_cliente.html', {'form': form})
 
+@login_required()
 def remover_cliente(request, id):
   cliente = Cliente.objects.get(id=id)
   if request.method == 'POST':
     cliente.delete()
     return redirect('listar_clientes')
   return render(request, 'clientes/confirma_exclusao.html', {'cliente': cliente}) 
+
 
 def cadastrar_usuario(request):
   if request.method == 'POST':
@@ -50,9 +57,25 @@ def cadastrar_usuario(request):
     form_usuario = UserCreationForm()
   return render(request, 'clientes/form_usuario.html', {'form_usuario': form_usuario})
 
+def logar_usuario(request):
+  if request.method == 'POST':
+    username = request.POST['username']
+    password = request.POST['password']
+    usuario = authenticate(request, username=username, password=password)
+    if usuario is not None:
+      login(request, usuario)
+      return redirect('listar_clientes')
+    else:
+      messages.error(request, 'As credenciais estão incorretas')
+      return redirect('logar_usuario')
+  else:
+    form_login = AuthenticationForm()
+  return render(request, 'clientes/form_login.html', {'form_login': form_login})
 
 
-
+def deslogar_usuario(request):
+  logout(request)
+  return redirect('logar_usuario')
 
 
 
